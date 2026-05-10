9005b1035ed18ffb4a5886bb9edbe920589b1016
鈴木 色音 <k024g0058@g.neec.ac.jp>
2026-05-10 17:04:38 +0900

    TL1 [確認課題] 01_05.Operator

--- BlenderScripts/LevelEditor/level_editor.py
+++ BlenderScripts/LevelEditor/level_editor.py
@@ -13,6 +13,34 @@ bl_info = {
     "category": "Object"
 }
 
+# オペレータ 頂点を伸ばす
+class MYADDON_OT_stretch_vertex(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_stretch_vertex"
+    bl_label = "頂点を伸ばす"
+    bl_description = "頂点座標を引っ張って伸ばします"
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    # メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
+        bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
+        print("頂点を伸ばしました。")
+        # オペレータの命令終了を通知
+        return {'FINISHED'}
+
+# オペレータ ICO球生成
+class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_create_ico_sphere"
+    bl_label = "ICO球生成"
+    bl_description = "ICO球を生成します"
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    # メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
+        bpy.ops.mesh.primitive_ico_sphere_add()
+        print("ICO球を生成しました。")
+        # オペレータの命令終了を通知
+        return {'FINISHED'}
+
 # トップバーの拡張メニュー
 class TOPBAR_MT_my_menu(bpy.types.Menu):
     # Blenderがクラスを識別する為の固有の文字列
@@ -24,11 +52,10 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
     
     # サブメニューの描画
     def draw(self, context):
-        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
-        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+        self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label)
         # 区切り線
         self.layout.separator()
-        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+        self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text=MYADDON_OT_create_ico_sphere.bl_label)
         # 区切り線
         self.layout.separator()
         self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
@@ -38,6 +65,8 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 
 classes = (
+    MYADDON_OT_stretch_vertex,
+    MYADDON_OT_create_ico_sphere,
     TOPBAR_MT_my_menu,
 )
 

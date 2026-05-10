a9906890f4fd7e2fdc296bd0c83dc894e134ddbd
鈴木 色音 <k024g0058@g.neec.ac.jp>
2026-05-10 16:48:32 +0900

    TL1 [確認課題] 01_04.Menu

--- BlenderScripts/LevelEditor/level_editor.py
+++ BlenderScripts/LevelEditor/level_editor.py
@@ -1,26 +1,64 @@
 import bpy
 
 bl_info = {
-    "name": "Level Editor",
-    "author": "Ion Suzuki",
+    "name": "レベルエディタ",
+    "author": "スズキ＿イオン",
     "version": (1, 0),
     "blender": (5, 1, 0),
     "location": "",
-    "desctiption": "Level Editor",
+    "description": "レベルエディタ",
     "warning": "",
     "wiki_url": "",
     "tracker_url": "",
     "category": "Object"
 }
 
-# addon enable callback
-def register():
-    print("Level Editor is enabled.")
-
-# addon disable callback
-def unregister():
-    print("Level Editor is disabled.")
+# トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    # Blenderがクラスを識別する為の固有の文字列
+    bl_idname = "TOPBAR_MT_my_menu"
+    # メニューのラベルとして表示される文字列
+    bl_label = "MyMenu"
+    # 著者表示用の文字列
+    bl_description = "拡張メニュー by" + bl_info["author"]
     
-# Test running code
+    # サブメニューの描画
+    def draw(self, context):
+        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+        # 区切り線
+        self.layout.separator()
+        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+        # 区切り線
+        self.layout.separator()
+        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+
+    # 既存のメニューにサブメニューを追加
+    def submenu(self, context):
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+classes = (
+    TOPBAR_MT_my_menu,
+)
+
+# アドオン有効化コールバック
+def register():
+    # Blenderにクラスを登録
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    # メニューに項目を追加
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
+    print("レベルエディタが有効化されました。")
+
+# アドオン無効化コールバック
+def unregister():
+    # メニューから項目を削除
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+    # Blenderからクラスを削除
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    print("レベルエディタが無効化されました。")
+
+# テストコード
 if __name__ == "__main__":
     register()

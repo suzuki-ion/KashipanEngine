1f989791644abe91eb4d749fa2e88b4f3a1b9d95
鈴木 色音 <k024g0058@g.neec.ac.jp>
2026-05-15 18:40:54 +0900

    ECS関連のファイル追加

---
+++ Project/KashipanEngine/Utilities/EntityComponentSystem.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "EntityComponentSystem/EntityDefinition.h"
+#include "EntityComponentSystem/EntityManager.h"
+#include "EntityComponentSystem/ComponentStrage.h"
+#include "EntityComponentSystem/SystemManager.h"
+#include "EntityComponentSystem/WorldECS.h"
---
+++ Project/KashipanEngine/Utilities/EntityComponentSystem/EntityDefinition.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <cstddef>
+
+namespace KashipanEngine {
+
+using Entity = std::size_t;
+
+} // namespace KashipanEngine

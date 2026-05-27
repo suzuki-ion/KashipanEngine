7c4e5b2c6891cda1f91eced75144bb97a9635f08
鈴木 色音 <k024g0058@g.neec.ac.jp>
2026-05-27 14:19:27 +0900

    BoxFilterを5x5固定だったのをCBによるサイズ指定で可変にした

--- Project/Assets/KashipanEngine/Pipeline/Preset/Shader/PostEffect/BoxFilterPS.hlsl
+++ Project/Assets/KashipanEngine/Pipeline/Preset/Shader/PostEffect/BoxFilterPS.hlsl
@@ -3,26 +3,25 @@
 Texture2D gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
-cbuffer BoxFilter5x5CB : register(b0) {
+cbuffer BoxFilterCB : register(b0) {
 	float2 gInvResolution;
 	float gIntensity;
-	float gPad;
+	float pad;
+	int2 halfSize;
 };
 
 float4 main(VSOutput input) : SV_Target0 {
 	float2 uv = saturate(input.uv);
 	float3 sum = 0.0f;
-
-	[unroll]
-	for (int y = -2; y <= 2; ++y) {
-		[unroll]
-		for (int x = -2; x <= 2; ++x) {
+	
+	for (int y = -halfSize.y; y <= halfSize.y; ++y) {
+		for (int x = -halfSize.x; x <= halfSize.x; ++x) {
 			float2 offset = float2(x, y) * gInvResolution;
 			sum += gTexture.Sample(gSampler, uv + offset).rgb;
 		}
 	}
 
-	float3 filtered = sum / 25.0f;
+	float3 filtered = sum / (float)( (2 * halfSize.x + 1) * (2 * halfSize.y + 1) );
 	float3 baseColor = gTexture.Sample(gSampler, uv).rgb;
 	float intensity = saturate(gIntensity);
 	float3 outColor = lerp(baseColor, filtered, intensity);

4de03b54ee5c712369d1fa5bd6cf2735590d20ae
鈴木 色音 <k024g0058@g.neec.ac.jp>
2026-06-30 10:30:14 +0900

    IObjectComponentのShowImGui、SaveToJson、LoadFromJsonをprotectedにし、それらを呼ぶためのインターフェース関数を用意。

--- Project/KashipanEngine/Objects/IObjectComponent.h
+++ Project/KashipanEngine/Objects/IObjectComponent.h
@@ -15,6 +15,7 @@
 namespace KashipanEngine {
 
 class EmptyObject;
+class SceneBase;
 class ObjectContext;
 class SceneContext;
 
@@ -38,26 +39,51 @@ public:
     size_t GetMaxComponentCountPerObject() const { return kMaxComponentCountPerObject_; }
     /// @brief コンポーネントの型IDを取得
     size_t GetComponentTypeID() const { return kComponentTypeID_; }
-
     /// @brief 更新優先度を取得
     int GetUpdatePriority() const { return updatePriority_; }
-    /// @brief 更新優先度を設定
-    void SetUpdatePriority(int priority) { updatePriority_ = priority; }
     /// @brief アクティブ状態を取得
     bool IsActive() const { return isActive_; }
     /// @brief アクティブ状態を設定
     void SetActive(bool active) { isActive_ = active; }
 
     /// @brief 初期化処理
-    void InitializeInterface(Passkey<EmptyObject>, ObjectContext *ownerObjectContext, SceneContext *sceneContext) {
-        objectContext_ = ownerObjectContext;
-        ownerSceneContext_ = sceneContext;
+    void InitializeInterface(Passkey<EmptyObject>, ObjectContext *objectContext, SceneContext *sceneContext) {
+        objectContext_ = objectContext;
+        sceneContext_ = sceneContext;
         Initialize();
     }
     /// @brief 終了処理
     void FinalizeInterface(Passkey<EmptyObject>) { Finalize(); }
     /// @brief 更新処理
     void UpdateInterface(Passkey<EmptyObject>) { if (IsActive()) { Update(); } }
+
+#ifdef USE_IMGUI
+    /// @brief ImGui 表示（ウィンドウの Begin/End は呼ばない）
+    void ShowImGuiInterface(Passkey<SceneBase>) { ShowImGui(); }
+#endif
+    JSON SaveToJsonInterface(Passkey<SceneBase>) const { return SaveToJson(); }
+    bool LoadFromJsonInterface(Passkey<SceneBase>, const JSON &json) { return LoadFromJson(json); }
+
+protected:
+    /// @brief コンポーネントの型IDを取得（派生クラスで使用する想定）
+    /// @tparam T コンポーネントの型
+    /// @return コンポーネントの型ID
+    template<typename T>
+    static size_t GetComponentTypeID() {
+        static size_t typeID = sComponentTypeID++;
+        return typeID;
+    }
+    IObjectComponent(const std::string &typeName, size_t maxCount, size_t componentTypeID)
+        : kComponentType_(typeName), kMaxComponentCountPerObject_(maxCount), kComponentTypeID_(componentTypeID), updatePriority_(1) {}
+#define OBJECT_COMPONENT_CONSTRUCTOR(typeName, maxCount, initializeCode) \
+    typeName() : IObjectComponent(#typeName, maxCount, GetComponentTypeID<typeName>()) { REGISTER_COMPONENT_OBJECT(typeName); initializeCode }
+
+    /// @brief 初期化処理
+    virtual void Initialize() {}
+    /// @brief 終了処理
+    virtual void Finalize() {}
+    /// @brief 更新処理
+    virtual void Update() {}
 
 #if defined(USE_IMGUI)
     /// @brief ImGui 表示（ウィンドウの Begin/End は呼ばない）
@@ -74,33 +100,13 @@ public:
     /// @return 成功した場合はtrue、失敗した場合はfalseを返す。読み込む情報がない場合は true を返す
     virtual bool LoadFromJson(const JSON &json) { (void)json; return true; }
 
-protected:
-    /// @brief 初期化処理
-    virtual void Initialize() {}
-    /// @brief 終了処理
-    virtual void Finalize() {}
-    /// @brief 更新処理
-    virtual void Update() {}
-
-    /// @brief コンポーネントの型IDを取得（派生クラスで使用する想定）
-    /// @tparam T コンポーネントの型
-    /// @return コンポーネントの型ID
-    template<typename T>
-    static size_t GetComponentTypeID() {
-        static size_t typeID = sComponentTypeID++;
-        return typeID;
-    }
-
-    IObjectComponent(const std::string &typeName, size_t maxCount, size_t componentTypeID)
-        : kComponentType_(typeName), kMaxComponentCountPerObject_(maxCount), kComponentTypeID_(componentTypeID), updatePriority_(1) {
-    }
-#define OBJECT_COMPONENT_CONSTRUCTOR(typeName, maxCount, initializeCode) \
-    typeName() : IObjectComponent(#typeName, maxCount, GetComponentTypeID<typeName>()) { REGISTER_COMPONENT_OBJECT(typeName); initializeCode }
+    /// @brief 更新優先度を設定
+    void SetUpdatePriority(int priority) { updatePriority_ = priority; }
 
     /// @brief 所属オブジェクトのコンテキストを取得
     ObjectContext *GetOwnerObjectContext() const { return objectContext_; }
     /// @brief 所属オブジェクトのシーンのコンテキストを取得
-    SceneContext *GetOwnerSceneContext() const { return ownerSceneContext_; }
+    SceneContext *GetOwnerSceneContext() const { return sceneContext_; }
 
 private:
     /// @brief コンポーネントの種類名
@@ -113,7 +119,7 @@ private:
     /// @brief オーナーオブジェクトのコンテキスト
     ObjectContext *objectContext_ = nullptr;
     /// @brief 所属シーンのコンテキスト
-    SceneContext *ownerSceneContext_ = nullptr;
+    SceneContext *sceneContext_ = nullptr;
 
     /// @brief 更新優先度（小さいほど先に更新される）
     int updatePriority_ = 1;
